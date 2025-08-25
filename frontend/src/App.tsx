7e83103456148e2960da1edf835214223bdd0b09
Faucon Noir <ytblackfalcon@gmail.com>
2025-08-25 19:44:28 +0200

    Configures tsoa to dynamically set API host
    Adds a pre-generation script to dynamically set the API host in the tsoa configuration based on the environment variable.
    This allows the swagger documentation to accurately reflect the deployed environment.
    Also updates nodemon to watch the pre-generation script for changes.
    
    Removes the static tsoa.json and adds @types/node dependency.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,13 +2,33 @@ import { useEffect } from "react";
 import { useTranslation } from "react-i18next";
 import "./App.css";
 import Pages from "./pages/Pages";
+import {
+	HelloApi,
+	Configuration
+} from './api/index';
+import { useNavigate } from "react-router-dom";
 
 function App() {
 	const { t } = useTranslation("common");
+	const navigate = useNavigate();
+	const configuration = new Configuration();
+	const apiInstance = new HelloApi(configuration);
 
 	useEffect(() => {
+		apiInstance.getHello()
+			.then(res => {
+				console.log(res.data.message);
+				if (!res.data.message) {
+					navigate("/404");
+				}
+			})
+			.catch(err => {
+				console.error("API error:", err);
+				navigate("/404");
+			});
+
 		document.title = t("title");
-	}, [t]);
+	}, [t, navigate]);
 
 	return (
 		// <Provider store={store}>
